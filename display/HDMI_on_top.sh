dd05e56a69c9896e6734331b0a8a1035b01974c2
Ivan Chan <ivan@ivanplex.com>
2018-05-29 22:10:36 +0100

    Display

---
+++ display/HDMI_on_top.sh
@@ -0,0 +1,4 @@
+xrandr --output HDMI-1 --auto --above eDP-1
+
+# Re-scale background
+feh --bg-center /home/ivan/Pictures/background.jpg /home/ivan/Pictures/background.jpg
---
+++ display/off.sh
@@ -0,0 +1,4 @@
+xrandr --output HDMI-1 --auto --off
+
+# Re-scale background
+feh --bg-center /home/ivan/Pictures/background.jpg /home/ivan/Pictures/background.jpg
